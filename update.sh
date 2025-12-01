04b6b32cd852e73cf764724efb18d9c21639dea9
Sergei Rozhkov <fgup@zserg.ru>
2025-12-01 12:39:21 +0500

    Update

--- update.sh
+++ update.sh
@@ -2,17 +2,32 @@
 
 source /opt/config/mod/.shell/0.sh
 
-if ! [ -f /ZMOD ]; then
-    a=$(readlink "/root/printer_data/gcodes" 2>/dev/null)
-    if [ "$a" != "$DATA_GCODES" ]; then
-        rm -rf /root/printer_data/gcodes/timelapse/tmp 2>/dev/null
-        rm -rf /root/printer_data/gcodes/timelapse 2>/dev/null
-        rmdir /root/printer_data/gcodes/gcodes 2>/dev/null
-        rm -f /root/printer_data/gcodes/gcodes 2>/dev/null
-        rmdir /root/printer_data/gcodes/ 2>/dev/null
-        rm -f /root/printer_data/gcodes 2>/dev/null
-    fi
-fi
+set -x
 
-/opt/config/mod/.shell/zremote.sh ln -s "$DATA_GCODES" /root/printer_data/gcodes 2>/dev/null
-ln -s /opt/config/mod_data/plugins/timelapse/timelapse.py /opt/config/mod/.shell/root/moonraker/components/timelapse.py 2>/dev/null
+update()
+{
+    ls -Rlh /root/printer_data
+
+    if ! [ -f /ZMOD ]; then
+        a=$(readlink "/root/printer_data/gcodes" 2>/dev/null)
+        if [ "$a" != "$DATA_GCODES" ]; then
+            rm -rf /root/printer_data/gcodes/timelapse
+
+            rmdir  /root/printer_data/gcodes/gcodes
+            rm -f  /root/printer_data/gcodes/gcodes
+
+            rmdir /root/printer_data/gcodes
+            rm -f /root/printer_data/gcodes
+        fi
+    fi
+
+    /opt/config/mod/.shell/zremote.sh ln -s "$DATA_GCODES" /root/printer_data/gcodes
+    a=$(readlink "/opt/config/mod/.shell/root/moonraker/components/timelapse.py" 2>/dev/null)
+    if [ "$a" != "/opt/config/mod_data/plugins/timelapse/timelapse.py" ]; then
+        ln -s /opt/config/mod_data/plugins/timelapse/timelapse.py /opt/config/mod/.shell/root/moonraker/components/timelapse.py
+    fi
+
+    ls -Rlh /root/printer_data
+}
+
+update &>${MOD_CONF}/mod_data/log/plugin_update_timelapse.log
