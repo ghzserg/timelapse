32f7af9366e9b951a07d5a58e007964a24b84e59
Sergei Rozhkov <fgup@zserg.ru>
2025-12-01 12:15:16 +0500

    Update

--- uninstall.sh
+++ uninstall.sh
@@ -3,12 +3,7 @@
 FILE="/opt/config/mod_data/plugins.moonraker.conf"
 
 sed -i '\|plugins/timelapse/moonraker\.timelapse\.cfg|d' "$FILE"
-/opt/config/mod/.shell/zremote.sh rm -rf /root/printer_data/gcodes/timelapse
-/opt/config/mod/.shell/zremote.sh rm -f /root/printer_data/gcodes/gcodes
-/opt/config/mod/.shell/zremote.sh rmdir /root/printer_data/gcodes/
-/opt/config/mod/.shell/zremote.sh rm -f /root/printer_data/gcodes
 /opt/config/mod/.shell/zremote.sh rm -f /opt/config/mod/.shell/root/moonraker/components/timelapse.py
-/opt/config/mod/.shell/zremote.sh ln -s "$DATA_GCODES" /root/printer_data/gcodes 2>/dev/null
 
 echo "Moonraker Timelapse Uninstalled"
 echo "REBOOT" >/tmp/printer
--- update.sh
+++ update.sh
@@ -3,10 +3,15 @@
 source /opt/config/mod/.shell/0.sh
 
 if ! [ -f /ZMOD ]; then
-    rm -rf /root/printer_data/gcodes/timelapse/tmp 2>/dev/null
-    rm -f /root/printer_data/gcodes/gcodes 2>/dev/null
-    rmdir /root/printer_data/gcodes/ 2>/dev/null
-    rm -f /root/printer_data/gcodes 2>/dev/null
+    a=$(readlink "/root/printer_data/gcodes" 2>/dev/null)
+    if [ "$a" != "$DATA_GCODES" ]; then
+        rm -rf /root/printer_data/gcodes/timelapse/tmp 2>/dev/null
+        rm -rf /root/printer_data/gcodes/timelapse 2>/dev/null
+        rmdir /root/printer_data/gcodes/gcodes 2>/dev/null
+        rm -f /root/printer_data/gcodes/gcodes 2>/dev/null
+        rmdir /root/printer_data/gcodes/ 2>/dev/null
+        rm -f /root/printer_data/gcodes 2>/dev/null
+    fi
 fi
 
 /opt/config/mod/.shell/zremote.sh ln -s "$DATA_GCODES" /root/printer_data/gcodes 2>/dev/null
