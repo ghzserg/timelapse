d37b4cde95d15a4a84aacaac54c94aaddbcae08b
Sergei Rozhkov <fgup@zserg.ru>
2025-12-01 12:06:17 +0500

    Update

--- uninstall.sh
+++ uninstall.sh
@@ -8,6 +8,7 @@ sed -i '\|plugins/timelapse/moonraker\.timelapse\.cfg|d' "$FILE"
 /opt/config/mod/.shell/zremote.sh rmdir /root/printer_data/gcodes/
 /opt/config/mod/.shell/zremote.sh rm -f /root/printer_data/gcodes
 /opt/config/mod/.shell/zremote.sh rm -f /opt/config/mod/.shell/root/moonraker/components/timelapse.py
+/opt/config/mod/.shell/zremote.sh ln -s "$DATA_GCODES" /root/printer_data/gcodes 2>/dev/null
 
 echo "Moonraker Timelapse Uninstalled"
 echo "REBOOT" >/tmp/printer
